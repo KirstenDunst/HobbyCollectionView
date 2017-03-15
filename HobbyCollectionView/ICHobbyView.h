505de6184b45042c19060d19c08fd2cf9b664770
KirstenDunst <cao_shixin@yahoo.com>
2017-03-15 18:05:46 +0800

    Add添加类型返回

--- HobbyCollectionView/ICHobbyView.h
+++ HobbyCollectionView/ICHobbyView.h
@@ -14,13 +14,14 @@ typedef void(^KindsChooseBlock)(NSString *kinds);
 
 @property(nonatomic , copy)KindsChooseBlock kindsBlock;
 
-/*  初始化方法        返回点击选中的类型
+
+
+/*  初始化方法        返回点击选中的类型字符串文字（需要其他类型返回，自定义）
  @param  frame                坐标尺寸大小
  @param  instailze_titleArr   初始显示标题数组
  @param  edit_titleArr        编辑状态显示的标题数组
  @param  hobbyArr             所有的可选类型数组组合  二级数组形式
  */
-
 
 - (instancetype)initWithFrame:(CGRect)frame andInstilizeTitleArr:(NSMutableArray *)instailze_titleArr andEditTitleArr:(NSMutableArray *)edit_titleArr andHobbyArr:(NSMutableArray *)hobbyArr AndbackForNewKinds:(KindsChooseBlock)block;
 
